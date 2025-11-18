95ed985c606a8063f924523594e62d9da117721a
shaman <baimurza.daulet@gmail.com>
2025-11-18 19:02:33 +0500

    Seed HAL init logic

--- devkit/sdk/hal_init.h
+++ devkit/sdk/hal_init.h
@@ -1,8 +1,12 @@
 #ifndef QAR_SDK_HAL_INIT_H
 #define QAR_SDK_HAL_INIT_H
 
-static inline void qar_sdk_init(void) {
-    /* Placeholder for future global init (e.g., enable interrupts) */
+#include "hal/uart.h"
+
+static inline void qar_sdk_init(void)
+{
+    /* Example init: ensure UART idle IRQs are disabled at boot. */
+    qar_uart_disable_irq(QAR_UART0_BASE, 0xFFFFFFFFu);
 }
 
 #endif /* QAR_SDK_HAL_INIT_H */
